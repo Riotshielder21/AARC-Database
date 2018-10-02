a716f3155f239e7991de39d565f9eec602542ae9
Riotshielder21 <Riotshielder21@users.noreply.github.com>
2018-10-02 21:38:29 +0100

    new file

--- Sub/Request.php
+++ Sub/Request.php
@@ -1,3 +1,11 @@
+<?php
+//Connect to the Databasee
+	$host="localhost";
+	$username="root";
+	$pword="";
+	$database="AARC";
+	require("..\phpCore\db_Connect.php");
+	?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,14 +26,84 @@
 			<div class="mItem"<div class="mItem"><a href="..\index.html">Home</a><div id="tMenuStat">|||</div></div>
 			<div class="mItem"><a href="Database.php">Database</a></div>
 			<div class="mItem"><a href="Network.html">Network</a></div>
-			<div class="mItem"><a href="Request.html">Request</a></div>
+			<div class="mItem"><a href="Request.php">Request</a></div>
 			<div class="mItem"><a href="Upload.php">Upload</a></div>
-			<div class="mItem" onclick="help()">Help</div>
 		</div>
 		
 		<div id="titleBlock">
 	
 			<h1 id="title">AARC Requests Form</h1>
-				<!--to fill in php commands for creating text file here-->
+	
 			<div class="mainBody">
+				<?php
+				
+	//initialise 
+	// $Username = '';
+	// $Password = '';
+	// $Submit = 0;
+		
+	// Check if the form was submitted
+	if(isset($_POST['title']) and isset($_POST['report'])){
+		
+		//POST Username and Password and report content
+		// $Username = filter_var(trim(stripslashes($_POST['Username'])));
+		// $Password = filter_var(trim(stripslashes($_POST['Password'])));
+		$title = filter_var(trim($_POST['title']));
+		$report = filter_var(trim($_POST['report']));
+
+		
+		//Get Username's Password
+		// $querystring= "SELECT userta.UserID, userta.Username, userta.Password, userta.Privilege, checkerta.CheckID FROM userta INNER JOIN checkerta ON userta.UserID = checkerta.UserID WHERE userta.Username = '".$Username."'";
+
+		//Send Query to MySQL
+		// if ($result = $mysqli->query($querystring)){
+						
+			//Fetch Row as Username Field is Unique there should only be 1 return
+			// while ($row = $result->fetch_object()){
+				// $confPass= $row->Password;
+						
+				//Check for password match before assigning the authority
+				// if ($confPass == $Password){
+					// $Submit = 1;
+				// }
+			// }
 			
+			//Check Authority
+			// If ($submit==1){
+				$title = $title . "\n";
+				//$fname = 'report' . date ('m:d-h:i');
+				//$fname = substr($title, 0, 2) . date ('m:d-h:i');
+				$reportfile = fopen("C:/Users/User/Desktop/".$fname.".txt", "w") or die("Unable to open file!");
+				fwrite($reportfile, $title);
+				fwrite($reportfile, $report);
+				fclose($reportfile);
+				echo "Upload Complete!";
+						
+			//Where user has no Authority, rejection handles
+			// } else{
+				// echo "For security reasons, Please contact a member with an account before making a Report!";
+			// }
+		// } else {
+			// echo "Failed to retrieve user details";
+		// }
+	} else {?>
+				<div class="mainBody" id="doubleBox">
+					<form method="Post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+						<!--<label id="usrform" name="Text">Please Enter Username and Password:</label><br />
+						<input type="text" name="Username" Placeholder="Username:"/><br />
+						<input type="password" name="Password" Placeholder="Password:"/><br /><br />-->
+						<label name="t" for="title">Title:</label>
+						<input type="Text" name="title" id="tbox"><br />
+						<label name="r" for="report">Report:</label>
+						<textarea form="usrform" name="report" id="report"><br />
+						<input type="submit" name="submit" Value="submit">
+					</form>
+<?php}?>
+			</div>
+		</div>
+	</div>
+</body>
+<footer>
+<?php echo date('d:m:y h:i:s');?>
+</footer>
+</html>
